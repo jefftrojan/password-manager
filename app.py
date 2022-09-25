03e0745a3369699f5f7a39ecd1d887b4cf13e237
Jeff Trojan <jefdauda@gmail.com>
2022-09-25 23:45:14 +0200

    Function to read passwords from file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-default_password = input("Enter admin password")
+default_password = input("Enter admin password: ")
 
-def view(): 
-    pass
+
+#function to add password to file
 def add():
     name = input("Account name: ")
     password = input("Password: ")
@@ -11,6 +11,20 @@ def add():
         # Write into the passwords.txt file
         f.write(name + " | " + password +"\n")
 
-add()
+# function read passwords
+def view():
+    with open("passwords.txt", "r") as f:
+        # Loop through the lines in the file and return output
+        for line in f.readlines():
+            print(line)
 
+
+def menu():
+    opt1 = 1
+    opt2 = 0
+    while opt1 == opt1:
+        return add()
+
+add()
+view()
 
